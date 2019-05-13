c79a036a71cd7da431e9b399a458ca6eb4df01b5
vicdubbeld <vicdubbeld@gmail.com>
2019-05-13 13:19:21 -0400

    establish connection between MySQL and js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,25 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// connection establishment
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "vivalabam1",
+    database: "bamazon"
+});
+
+// connection validation
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+
